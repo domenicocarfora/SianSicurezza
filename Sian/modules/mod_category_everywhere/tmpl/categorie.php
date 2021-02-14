756910debe8fd4afb3c1b33ad2e9f2ccd99ec6ab
domenicocarfora <domenico.carfora@outlook.it>
2021-02-14 20:53:14 +0100

    aggiunte immagini categoria e fix vari alla visualizzazione

--- Sian/modules/mod_category_everywhere/tmpl/categorie.php
+++ Sian/modules/mod_category_everywhere/tmpl/categorie.php
@@ -1,15 +1,17 @@
 <?php
 defined('_JEXEC') or die;
 ?>
+
 <div class="row">
-<div class="img_partner">
+
+<div class="img_category">
     <?php
     foreach ($categorie as $categoria){
         if ($categoria->parent=='0'){
         $parametri=json_decode($categoria->params,true);
-        echo "<div class='col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4'>";
+        echo "<div class='col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 imgcat'>";
         echo "<a href='".JURI::root(). $categoria->path."'>";
-        echo "<span>$categoria->name</span>";
+        echo "<span class='catname'><b>$categoria->name</b></span>";
         echo "<img class='catlogo' src='". JURI::root().$parametri['content.image']. "' alt='$categoria->name'>";
         echo "</a>";
         echo "</div>";}}
