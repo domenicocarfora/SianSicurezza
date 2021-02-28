f0f682b5db661e455a4c2cd0c13e635fec272e2d
domenicocarfora <domenico.carfora@outlook.it>
2021-02-28 14:59:47 +0100

    aggiornata visualizzazione carrello full con tabella

--- Sian/modules/mod_carrello/tmpl/full.php
+++ Sian/modules/mod_carrello/tmpl/full.php
@@ -5,12 +5,15 @@ defined('_JEXEC') or die;
 <div id="itemcarrello">
 
             <?php if(count($carrello)>0){
-                echo "<div><table>
-        <tr><td>Nome prodotto</td><td>Quantità</td><td>Opzioni</td></tr>";
+                echo "<div><table id='listcarrello' style='width: 50%'>
+        <tr style='height: 50px'><td>Nome prodotto</td><td>Quantità</td><td>Opzioni</td></tr>";
                 foreach ($carrello as $item){
-                    echo "<tr id='shopitem-".$item->id_prodotto."'><td><span>".$item->name."</span></td>   <td><span> <input data-id_prod=".$item->id_prodotto." value=".$item->quantita." type='number' id='quantity' name='quantity'/></span> </td>  <td><span><button type=button class=remove aria-label=rimuovi data-id='".$item->id_prodotto."'> X </button></span></td></li>";
+                    echo "<tr id='shopitem-".$item->id_prodotto."' style='height: 50px'>
+<td><span>".$item->name."</span></td>   
+<td><span> <input class='quantity' data-id_prod=".$item->id_prodotto." value=".$item->quantita." type='number' id='quantity' name='quantity'/></span> </td>  
+<td><span><img class='removebuttonfull remove button' src='images/icone/elimina.svg' aria-label=rimuovi data-id='".$item->id_prodotto."'></span></td></tr>";
                 };
-                echo "</ul></div>";
+                echo "</div>";
             }else{
                 echo "<span>Il carrello è vuoto</span>";
             } ?>
@@ -98,14 +101,13 @@ defined('_JEXEC') or die;
                 removeItem(id_prodotto);
         });
 
-        jQuery('#quantity').change(function() {
+        jQuery('.quantity').change(function() {
             quantita=this.value;
              id_prodotto=($(this).data('id_prod'));
              updateItem(id_prodotto,quantita);
         });
 
         jQuery('#send').click(function() {
-            //console.log('ciao');
             sendcarrello();
         });
 
