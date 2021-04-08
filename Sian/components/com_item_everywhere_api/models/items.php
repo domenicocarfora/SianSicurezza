c11e76ae68d6a0a89bf40b9d862210f4437ef4fd
domenicocarfora <domenico.carfora@outlook.it>
2021-04-08 21:24:11 +0200

    aggiunta pagination alla visualizzazione degli item

--- Sian/components/com_item_everywhere_api/models/items.php
+++ Sian/components/com_item_everywhere_api/models/items.php
@@ -19,12 +19,13 @@ class Item_everywhere_apiModelItems extends JModelList
     public function getItems()
     {
         $categories=json_decode(JFactory::getApplication()->input->json->getRaw())->cat;
+        $last=json_decode(JFactory::getApplication()->input->json->getRaw())->last;
         $query = self::getItemsByCategoryQuery($categories);
         $db = JFactory::getDbo();
-        $db->setQuery($query, 0);
+        $db->setQuery($query);
         $ids = $db->loadColumn();
         if (!empty($ids)){
-        $items= self::getByIds($ids);
+        $items= self::getByIds($ids,$last);
         echo json_encode($items);
         exit();}else {
             echo json_encode(array("msg"=>"no items"));exit();
@@ -32,14 +33,14 @@ class Item_everywhere_apiModelItems extends JModelList
 
     }
 
-    static function getByIds($ids){
+    static function getByIds($ids,$last=0){
         $db = JFactory::getDBO();
         $query="SELECT DISTINCT jzi.id,jzi.name,jzi.alias,
                 REPLACE(JSON_EXTRACT(JSON_EXTRACT(jzi.elements,'$.\"c26feca6-b2d4-47eb-a74d-b067aaae5b90\"'),'$.\"file\"'),'\"','') as immagine,
                 REPLACE(JSON_EXTRACT(JSON_EXTRACT(JSON_EXTRACT(elements,'$.\"08795744-c2dc-4a68-8252-4e21c4c4c774\"'),'$.\"0\"'),'$.\"value\"'),'\"','') as sottotitolo,
                 REPLACE(JSON_EXTRACT(JSON_EXTRACT(JSON_EXTRACT(elements,'$.\"e59e537d-79f3-46d6-8c98-1ef606d98eba\"'),'$.\"0\"'),'$.\"value\"'),'\"','') as short_desc
                 FROM #__zoo_item jzi
-                WHERE jzi.application_id=1 AND (publish_down IS NULL OR publish_down>sysdate()) AND jzi.id in (".implode(',', $ids).");";
+                WHERE jzi.application_id=1 AND (publish_down IS NULL OR publish_down>sysdate()) AND jzi.id in (".implode(',', $ids).") ORDER BY jzi.id LIMIT ".$last.",10;";
 
         $db->setQuery($query);
         $items = $db->loadObjectList();
@@ -48,7 +49,7 @@ class Item_everywhere_apiModelItems extends JModelList
             $item->short_desc=str_replace('\r\n',' ',$item->short_desc);
             $items_def[]=$item;
         }
-        return $items;
+        return $items_def;
     }
 
 
--- Sian/modules/mod_item_everywhere/tmpl/default.php
+++ Sian/modules/mod_item_everywhere/tmpl/default.php
@@ -11,8 +11,9 @@ foreach ($filter as $filtro){
 echo "</div><br>";
 echo "<div><span id='apply_filter' class='btn btn-primary'>Applica filtri</span></div>";
 echo "</br><hr>";
-echo "<div id='loading' style='display:block;'><img src=".JUri::base()."images/gifs/wait.gif></div>";
 echo "<div id='container_items'></div>";
+echo "<div id='loading' style='display:block;'><img src=".JUri::base()."images/gifs/wait.gif></div>";
+echo "<div class='pagination' style='display:none;'><a class='pag_list'>Mostra Altro</a></div>";
 echo "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>";
 echo "<script type='application/javascript'>
 
@@ -22,6 +23,12 @@ jQuery('#apply_filter').click(function() {
     jQuery('#container_items').empty();
      jQuery('#loading').css('display', 'block');
              getitems();
+     jQuery('.pagination').css('display', 'block');
+        });
+
+jQuery(document).on('click','.pag_list', function() {
+    jQuery('#loading').css('display', 'block');
+             moreItems();
         });
 });
 
@@ -35,17 +42,23 @@ function getitems() {
                 if ($( this ).val() != 'none'){
                filters.push(parseInt($( this ).val()));}
             });
+            if(jQuery('.items_list > div').length==0){
+                var x=0;
+            }else{
+            var x=jQuery('.items_list > div').last().attr('id').split('-')[1];
+            jQuery('#container_items').empty();
+            }
             jQuery.ajax({
                 url: 'index.php?option=com_item_everywhere_api&task=items.getItems',
-                data: JSON.stringify({cat: filters}),
+                data: JSON.stringify({cat: filters, last: x}),
                 dataType: 'json',
                 method: 'POST',
                 contentType: 'application/json',
                 success: function (data) {
-                    if (data['msg']){
+                    if (data=='' || !data){
                         var test='<div id=\"no_items\">Nessun elemento trovato</div>';
                     }else{
-                        var test= '<id class=\"items_list\">';
+                        var test= '<div class=\"items_list\">';
                     data.forEach (function(item,index) {
                       test+= '<div class=\"items_ls item-'+
                       item['id'] + '\" id=\"item-' + 
@@ -56,18 +69,58 @@ function getitems() {
                       item['sottotitolo'] + '</h3></div></a><div class=\"short_desc\">'+ item['short_desc'] +'</div></div></div><hr>';
                     })
                     test+='</div>';
-                    test+='<ul class=\"pagination\">';
-                    for (var i=1; i<=28; i++)
-                    test+='<li  class=\"pag_list\"><a class=\"pag_link\" href=\"#\">'+ i +'</a></li>';
-                    test+='</ul>';
                     }
                     jQuery('#container_items').append(test);
+                    jQuery('.pagination').css('display', 'block');
                 },
         complete: function() {
             $('#loading').css('display', 'none');
         }
                 })
 }
-</script>
 
+function moreItems() {
+    var filtri=jQuery('select.filter');     
+           var filters=[];
+           filters.push(".$cat_madre.");
+            jQuery.each(filtri,function(){
+                if ($( this ).val() != 'none'){
+               filters.push(parseInt($( this ).val()));}
+            });
+            if(jQuery('.items_list > div').length==0){
+                var x=0;
+            }else{
+            var x=jQuery('.items_list > div').last().attr('id').split('-')[1];
+            }
+            jQuery.ajax({
+                url: 'index.php?option=com_item_everywhere_api&task=items.getItems',
+                data: JSON.stringify({cat: filters, last: x}),
+                dataType: 'json',
+                method: 'POST',
+                contentType: 'application/json',
+                success: function (data) {
+                    if (data=='' || !data){
+                        var test='<div id=\"no_items\">Non ci sono nuovi elementi</div>';
+                        $('.pagination').css('display', 'none');
+                    }else{
+                        var test='';
+                    data.forEach (function(item,index) {
+                      test+= '<div class=\"items_ls item-'+
+                      item['id'] + '\" id=\"item-' + 
+                      item['id'] + '\"><a class=\"no-before\" href=\"".JUri::base()."component/zoo/item/' + 
+                      item['alias'] + '\"><div class=\"img_item\"><img class=\"prodotto_immagine\" src=\"".JUri::base()."' + 
+                      item['immagine']+'\"></div><div class=\"description\"><div id=\"item_name\"><h1>' + 
+                      item['name'] + '</h1></div><div id=\"item_subtitle\"><h3>' + 
+                      item['sottotitolo'] + '</h3></div></a><div class=\"short_desc\">'+ item['short_desc'] +'</div></div></div><hr>';
+                    })
+                    }
+                    jQuery('.items_list').append(test);
+                },
+        complete: function() {
+            $('#loading').css('display', 'none');
+        }
+                })
+}
+
+</script>
 ";
