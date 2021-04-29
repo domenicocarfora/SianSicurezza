1a1c15d5da6c9502fbc0139505b83c6014548c4d
Domenico Carfora <d.carfora.c@logotel.it>
2021-04-29 18:45:50 +0200

    modificato visualizzazione delle immagini dei partner

--- modules/mod_brand_partner/tmpl/default.php
+++ modules/mod_brand_partner/tmpl/default.php
@@ -5,7 +5,7 @@ defined('_JEXEC') or die;
 <div class="img_partner">
     <?php
     foreach ($partners as $partner){
-        echo "<div class='col-xs-6 col-sm-6 col-md-3 col-lg-2 col-xl-1'>";
+        echo "<div class='img_col_brand col-xs-6 col-sm-6 col-md-3 col-lg-2 col-xl-1'>";
         if (isset($partner->link) && $partner->link!=''){
             echo "<a target='_blank' href='".$partner->link. "'>";
         }
